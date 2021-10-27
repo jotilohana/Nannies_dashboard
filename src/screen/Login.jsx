9e2a54596ae28c21d0b4ef21a0a60a53fa64b46f
jotilohana <jklohana208@gmail.com>
2021-10-27 22:21:59 +0500

    Changes

--- src/screen/Login.jsx
+++ src/screen/Login.jsx
@@ -1,32 +1,44 @@
 import React from 'react';
 import LoginImage from '../Images/Login_side_image.jpg';
-import {Container, Row, Col} from 'react-bootstrap';
+import { Row, Col, Form, Button } from 'react-bootstrap';
+import '../App.css';
 
-function Login(){
-    return(
-        <Container className="Login_main_container">
-        <Row className="justify-content-md-center">
-          <Col xs lg="5">
-             <img className="image_login" src={LoginImage} alt="Side Image" />
-           </Col>
-          <Col md="auto">
-          <form className="login-div col-md-4 mb-3">
-        <h3>ATTENDANCE SYSTEM</h3>
-        <h4>LOG IN</h4>
-        <div className="form-group input ">
-          <input type="email" placeholder=" Email" required className="form-control fas fa-envelope-square input" id="exampleInputEmail1" aria-describedby="emailHelp" /><br /><br />
-        </div>
-        <div className="form-group ">
-          <input className="input" type="password" placeholder=" Password" required className="form-control fas fa-key input" id="exampleInputPassword1" />
-        </div>
-        <div>
-          <button type="button" className="btn btn-success">LOGIN</button>
-        </div>
-      </form>
-          </Col>
-        </Row>  
-      </Container>
-    );
+function Login() {
+  return (
+
+    <div className="Login_main_container">
+      <Row className="firstdiv">
+        <Col xs lg="5">
+          <img className="image_login" src={LoginImage} alt="Side Image" />
+        </Col>
+        <Col md="auto" xs={6}>
+          <h1 className="h1tag">Admin Portal</h1>
+          <div className="login_div">
+          <h4 className="h4tag">LOG-IN</h4>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <p><a className="link" href="">Forgot Password</a></p>
+              
+            </Form.Group>
+            <Button className="btn" variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+          </div>
+        </Col>
+      </Row>
+    </div>
+  );
 }
 
 export default Login();
